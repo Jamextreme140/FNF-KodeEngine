6cd7aef6a65b51aceae9f87a71c85df4b8f8e2a4
Jaime Humberto Macias Bustamante <jamextreme140@outlook.com>
2024-01-18 22:42:44 -0700

    set version and fix workflow

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -10,6 +10,7 @@ import options.OptionsState;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.7.3'; // This is also used for Discord RPC
+	public static var kodeEngineVersion:String = '0.1.0'; // (just like the previous said)
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
@@ -87,6 +88,10 @@ class MainMenuState extends MusicBeatState
 			menuItem.screenCenter(X);
 		}
 
+		var kodeVer:FlxText = new FlxText(12, FlxG.height - 64, 0, "Kode Engine v" + kodeEngineVersion, 12);
+		kodeVer.scrollFactor.set();
+		kodeVer.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(kodeVer);
 		var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		psychVer.scrollFactor.set();
 		psychVer.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
